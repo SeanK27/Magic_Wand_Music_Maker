9ef2a50e8d3357ebfc4d3f0215b8e36cf4064081
learner559 <143897809+learner559@users.noreply.github.com>
2023-11-04 22:30:03 -0600

    Add save to file Capitalized constants and added a save to file feature

--- productionScripts/TestCodeforMotionInput.py
+++ productionScripts/TestCodeforMotionInput.py
@@ -18,11 +18,11 @@ pygame.mixer.init(channels=4)
 pygame.mixer.Channel(0)
 
 # Serial baud rate to sample WitMotion data
-baud = 9600
+BAUD = 9600
 
 # Minimum velocity threshold to signal that IMU is in deliberate motion
 # Change when needed, if IMU is too sensitive, increase this value and vice versa
-threshold = 15
+THRESHOLD = 15
 
 
 # Calculate magnitude of net velocity vector using distance equation d = sqrt(x^2 + y^2 + z^2)
@@ -30,25 +30,46 @@ def get_net_velocity(velocity_list):
     return math.sqrt((velocity_list[0] ** 2) + (velocity_list[1] ** 2) + velocity_list[2] ** 2)
 
 
-with serial.Serial("COM6", baud, timeout=5) as ser:
+RECORD_DATA_IN_FILE = True
+
+# Create data files if they don't exist
+if RECORD_DATA_IN_FILE == True:
+    temp = open("gyro-output.txt", "a+")
+    temp.close()
+    temp = open("angle-output.txt", "a+")
+    temp.close()
+    del temp
+
+with serial.Serial("COM6", BAUD, timeout=5) as ser:
     # Begin channels of music.
     # 'track#': corresponding track; 'loops=-1': loop indefinitely; 'fade_ms=1000': fade in and out 1sec
     pygame.mixer.Channel(0).play(pygame.mixer.Sound(file=track1), loops=-1, fade_ms=1000)
 
     while True:
-        # Read byte of data from IMU until start bit(0x55 or U in ASCII)
+        # Open data files
+        if RECORD_DATA_IN_FILE == True:
+            gyro_file = open("gyro-output.txt", "a")
+            angle_file = open("angle-output.txt", "a")
+        # Read byte of data from IMU until start bit (0x55 or U in ASCII)
         s = ser.read_until(b'U')
 
         # Get gyro and angle values while ignoring None-type values and storing in velocity and angle variable
         if wit.get_gyro(s) is not None:
             velocity = wit.get_gyro(s)
+            if RECORD_DATA_IN_FILE == True:
+                gyro_file.write(velocity[0] + " " + velocity[1] + " " + velocity[2])
 
         if wit.get_angle(s) is not None:
             angle = wit.get_angle(s)
+            if RECORD_DATA_IN_FILE == True:
+                angle_file.write(angle[0] + " " + angle[1] + " " + angle[2])
 
         # Check whether the IMU is moving.
         # Play music when velocity > threshold and pause when <= threshold
-        if get_net_velocity(velocity) > threshold:
+        if get_net_velocity(velocity) > THRESHOLD:
             pygame.mixer.Channel(0).unpause()
-        elif get_net_velocity(velocity) <= threshold:
+        elif get_net_velocity(velocity) <= THRESHOLD:
             pygame.mixer.Channel(0).pause()
+        if RECORD_DATA_IN_FILE == True:
+            gyro_file.close()
+            angle_file.close()
