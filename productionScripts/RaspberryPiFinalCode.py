5f65dd55a1ee5049bb4e61271f7d2337788e693c
SeanK27 <seankk100@gmail.com>
2023-11-14 10:49:07 -0700

    This code works! Added 4 button functionality and made some edits to work with new PCB.

--- productionScripts/RaspberryPiFinalCode.py
+++ productionScripts/RaspberryPiFinalCode.py
@@ -8,10 +8,10 @@ import time
 
 # Setup GPIO Inputs
 GPIO.setmode(GPIO.BOARD)
-buttonPin0 = 32
-buttonPin1 = 16
-buttonPin2 = 18
-buttonPin3 = 22
+buttonPin0 = 16
+buttonPin1 = 18
+buttonPin2 = 22
+buttonPin3 = 24
 GPIO.setup(buttonPin0, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(buttonPin1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(buttonPin2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -22,10 +22,10 @@ velocity = [0, 0, 0]
 angle = [0, 0, 0]
 
 # Song tracks with corresponding file paths. Stored in 'songs' folder named 'track#'
-track1 = 'songs/Melody_Track.mp3'
-track2 = 'songs/Chords_Track.mp3'
-track3 = 'songs/Drums_Track.mp3'
-track4 = 'songs/Bass_Track.mp3'
+track1 = 'songs/Melody1.wav'
+track2 = 'songs/Chords1.wav'
+track3 = 'songs/Drums1.wav'
+track4 = 'songs/Bass1.wav'
 
 # Setup pygame mixer and channels
 mixer.init(channels=4)
@@ -39,7 +39,7 @@ baud = 9600
 
 # Minimum velocity threshold to signal that IMU is in deliberate motion
 # Change when needed, if IMU is too sensitive, increase this value and vice versa
-threshold = 20
+threshold = 10
 
 
 # Calculate magnitude of net velocity vector using distance equation d = sqrt(x^2 + y^2 + z^2)
@@ -52,8 +52,8 @@ with serial.Serial("/dev/ttyS0", baud, timeout=5) as ser:
     # 'track#': corresponding track; 'loops=-1': loop indefinitely; 'fade_ms=1000': fade in and out 1sec
     mixer.Channel(0).play(mixer.Sound(file=track1), loops=-1)
     mixer.Channel(1).play(mixer.Sound(file=track2), loops=-1)
-    ##    mixer.Channel(2).play(mixer.Sound(file=track3), loops=-1)
-    ##    mixer.Channel(3).play(mixer.Sound(file=track4), loops=-1)
+    mixer.Channel(2).play(mixer.Sound(file=track3), loops=-1)
+    mixer.Channel(3).play(mixer.Sound(file=track4), loops=-1)
 
     while True:
         # Read byte of data from IMU until start bit(0x55 or U in ASCII)
@@ -71,14 +71,14 @@ with serial.Serial("/dev/ttyS0", baud, timeout=5) as ser:
         if get_net_velocity(velocity) > threshold:
             mixer.Channel(0).unpause()
             mixer.Channel(1).unpause()
-        ##            mixer.Channel(2).unpause()
-        ##            mixer.Channel(3).unpause()
+            mixer.Channel(2).unpause()
+            mixer.Channel(3).unpause()
 
         elif get_net_velocity(velocity) <= threshold:
             mixer.Channel(0).pause()
             mixer.Channel(1).pause()
-        ##            mixer.Channel(2).pause()
-        ##            mixer.Channel(3).pause()
+            mixer.Channel(2).pause()
+            mixer.Channel(3).pause()
 
         # Signal for whether the reference angle has been set
         taredangle = False
@@ -87,25 +87,26 @@ with serial.Serial("/dev/ttyS0", baud, timeout=5) as ser:
             # Reads serial data and stores velocity and angle
             mixer.Channel(0).unpause()
             mixer.Channel(1).unpause()
-            ##            mixer.Channel(2).unpause()
-            ##            mixer.Channel(3).unpause()
+            mixer.Channel(2).unpause()
+            mixer.Channel(3).unpause()
             s = ser.read_until(b'U')
             if wit.get_angle(s) is not None:
                 angle = wit.get_angle(s)
 
             # Sets reference angle of the IMU
             if not taredangle:
+                print("button0 pressed")
                 angley_start = angle[1]
                 taredangle = True
 
             # When the difference between the reference angle and the actual angle is greater than 5 (wand is rotated)
             if angle[1] - angley_start > 0 and abs(angle[1] - angley_start) > 10:
-                mixer.Channel(0).set_volume(mixer.Channel(0).get_volume() + 0.04)
+                mixer.Channel(0).set_volume(mixer.Channel(0).get_volume() - 0.04)
                 print("Volume0: " + str(mixer.Channel(0).get_volume()))
                 print("Angle: " + str(angle[1]))
 
             if angle[1] - angley_start < 0 and abs(angle[1] - angley_start) > 10:
-                mixer.Channel(0).set_volume(mixer.Channel(0).get_volume() - 0.04)
+                mixer.Channel(0).set_volume(mixer.Channel(0).get_volume() + 0.04)
                 print("Volume: " + str(mixer.Channel(0).get_volume()))
                 print("Angle: " + str(angle[1]))
 
@@ -113,8 +114,35 @@ with serial.Serial("/dev/ttyS0", baud, timeout=5) as ser:
             # Reads serial data and stores velocity and angle
             mixer.Channel(0).unpause()
             mixer.Channel(1).unpause()
-            ##            mixer.Channel(2).unpause()
-            ##            mixer.Channel(3).unpause()
+            mixer.Channel(2).unpause()
+            mixer.Channel(3).unpause()
+            s = ser.read_until(b'U')
+            if wit.get_angle(s) is not None:
+                angle = wit.get_angle(s)
+
+            # Sets reference angle of the IMU
+            if not taredangle:
+                print("button1 pressed")
+                angley_start = angle[1]
+                taredangle = True
+
+            # When the difference between the reference angle and the actual angle is greater than 5 (wand is rotated)
+            if angle[1] - angley_start > 0 and abs(angle[1] - angley_start) > 10:
+                mixer.Channel(1).set_volume(mixer.Channel(1).get_volume() - 0.04)
+                print("Volume1: " + str(mixer.Channel(1).get_volume()))
+                print("Angle: " + str(angle[1]))
+
+            if angle[1] - angley_start < 0 and abs(angle[1] - angley_start) > 10:
+                mixer.Channel(1).set_volume(mixer.Channel(1).get_volume() + 0.04)
+                print("Volume: " + str(mixer.Channel(1).get_volume()))
+                print("Angle: " + str(angle[1]))
+
+        while GPIO.input(buttonPin2) == 0:
+            # Reads serial data and stores velocity and angle
+            mixer.Channel(0).unpause()
+            mixer.Channel(1).unpause()
+            mixer.Channel(2).unpause()
+            mixer.Channel(3).unpause()
             s = ser.read_until(b'U')
             if wit.get_angle(s) is not None:
                 angle = wit.get_angle(s)
@@ -123,14 +151,42 @@ with serial.Serial("/dev/ttyS0", baud, timeout=5) as ser:
             if not taredangle:
                 angley_start = angle[1]
                 taredangle = True
+                print("button2 pressed")
 
             # When the difference between the reference angle and the actual angle is greater than 5 (wand is rotated)
             if angle[1] - angley_start > 0 and abs(angle[1] - angley_start) > 10:
-                mixer.Channel(1).set_volume(mixer.Channel(1).get_volume() + 0.04)
-                print("Volume1: " + str(mixer.Channel(1).get_volume()))
+                mixer.Channel(2).set_volume(mixer.Channel(2).get_volume() - 0.04)
+                print("Volume1: " + str(mixer.Channel(2).get_volume()))
                 print("Angle: " + str(angle[1]))
 
             if angle[1] - angley_start < 0 and abs(angle[1] - angley_start) > 10:
-                mixer.Channel(1).set_volume(mixer.Channel(1).get_volume() - 0.04)
-                print("Volume: " + str(mixer.Channel(1).get_volume()))
+                mixer.Channel(2).set_volume(mixer.Channel(2).get_volume() + 0.04)
+                print("Volume: " + str(mixer.Channel(2).get_volume()))
+                print("Angle: " + str(angle[1]))
+
+        while GPIO.input(buttonPin3) == 0:
+            # Reads serial data and stores velocity and angle
+            mixer.Channel(0).unpause()
+            mixer.Channel(1).unpause()
+            mixer.Channel(2).unpause()
+            mixer.Channel(3).unpause()
+            s = ser.read_until(b'U')
+            if wit.get_angle(s) is not None:
+                angle = wit.get_angle(s)
+
+            # Sets reference angle of the IMU
+            if not taredangle:
+                angley_start = angle[1]
+                taredangle = True
+                print("button3 pressed")
+
+            # When the difference between the reference angle and the actual angle is greater than 5 (wand is rotated)
+            if angle[1] - angley_start > 0 and abs(angle[1] - angley_start) > 10:
+                mixer.Channel(3).set_volume(mixer.Channel(3).get_volume() - 0.04)
+                print("Volume1: " + str(mixer.Channel(3).get_volume()))
+                print("Angle: " + str(angle[1]))
+
+            if angle[1] - angley_start < 0 and abs(angle[1] - angley_start) > 10:
+                mixer.Channel(3).set_volume(mixer.Channel(3).get_volume() + 0.04)
+                print("Volume: " + str(mixer.Channel(3).get_volume()))
                 print("Angle: " + str(angle[1]))
